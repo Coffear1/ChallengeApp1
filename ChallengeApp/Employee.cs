acc0e5b468eebef673a967fa5e60559e3548a44a
Krzysiek <krzysiek.stebel@gmail.com>
2023-02-01 19:23:10 +0100

    zadanie domowe dzień 6

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string imię, string nazwisko, string wiek)
+        {
+            this.Imię = imię;
+            this.Nazwisko = nazwisko;
+            this.Wiek = wiek;
+        }
+
+        public string Imię { get; private set; }
+        public string Nazwisko { get; private set; }
+        public string Wiek { get; private set; }
+
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
