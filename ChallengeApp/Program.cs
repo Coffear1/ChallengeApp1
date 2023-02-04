03260e601b42a65322502dfa00fd9f1aa7f72ea4
Krzysiek <krzysiek.stebel@gmail.com>
2023-02-04 12:15:02 +0100

    zadanie domowe dzien 6

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -13,9 +13,9 @@ employee1.AddScore(8);
 
 employee2.AddScore(4);
 employee2.AddScore(8);
-employee2.AddScore(9);       // łącznie 33pkt
+employee2.AddScore(5);       // łącznie 31pkt
 employee2.AddScore(5);
-employee2.AddScore(7);
+employee2.AddScore(9);
 
 employee3.AddScore(4);
 employee3.AddScore(6);
@@ -33,20 +33,10 @@ Employee employeeWithMaxResult = null;
 
 foreach(var employee in employees)
 {
-    if(employee1.Result > maxResult)
+    if(employee.Result > maxResult)
     {
-        employeeWithMaxResult = employee1;
-        maxResult = employee1.Result; 
-    }
-    else if(employee2.Result > maxResult)
-    {
-        employeeWithMaxResult = employee2;
-        maxResult = employee2.Result;
-    }
-    else if(employee3.Result > maxResult)
-    {
-        employeeWithMaxResult = employee3;
-        maxResult = employee3.Result;
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result; 
     }
 }
 Console.WriteLine("Pracownik z najwyższą liczbą punktów:" + " " + employeeWithMaxResult.Imię + " " + employeeWithMaxResult.Nazwisko + " " + maxResult + "pkt" );
